15c7c36034ed2a6446bf0a05053d9fa739af6253
Andrew McGuan <amcguan@calpoly.edu>
2018-02-08 16:16:29 -0800

    same as the last commit but now it actually works

--- tractordata_parse/runall.sh
+++ tractordata_parse/runall.sh
@@ -4,6 +4,6 @@ for fullfile in data/*.csv; do
 	name=${fullfile##*/}
 	file=${name%.csv}
 	mkdir filedata/"$file"
-	./main.py --collate > filedata/"$file"/"$file""_collate.txt"
-	./main.py --dump-ids > filedata/"$file"/"$file""_dumpids.txt"
+	./main.py $fullfile --collate > filedata/"$file"/"$file""_collate.txt"
+	./main.py $fullfile --dump-ids > filedata/"$file"/"$file""_dumpids.txt"
 done
