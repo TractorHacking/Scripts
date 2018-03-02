80ab199076ef97ffbb28cef9b091d4ad83ab31c5
Tim Letz <tim.letz@comcast.net>
2018-03-01 16:31:22 -0800

    Not sure if this is jiggered up right but hwg

---
+++ tractordata_parse/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+
+
+setup(
+  name="tractordata_parse",
+  version="0.0.1",
+  author="Tim Letz",
+  install_requires=[
+    "sqlalchemy==1.2", 
+    "openpyxl==2.5.0",
+    "bitstruct==3.7.0"
+  ],
+  packages=find_packages('src'),
+  package_dir={'':'src'},
+  entry_points={
+    'console_scripts': [
+      'tractordata_parse = tractordata_parse:main'
+    ]
+  },
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Topic :: Utilities",
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator"
+  ],
+  test_suite='tests.my_test_suite'
+)
